2ddb4a86970245a555b633403682716c80533077
jefrecantuledesma <98501869+jefrecantuledesma@users.noreply.github.com>
2024-01-25 14:10:22 -0500

    Initial upload
    Initial upload, will shutdown computer using systemctl when it detects a usb device.

---
+++ main.py
@@ -0,0 +1,35 @@
+import pyudev
+import subprocess
+
+def device_event(observer, device):
+    action = device.action
+    if action == "add":
+        print(f"USB Device Plugged In: {device}")
+        power_off()
+    elif action == "remove":
+        print(f"USB Device Unplugged: {device}")
+        power_off()
+
+def power_off() -> None:
+    print("Powering off the computer.")
+    #UNCOMMENT WHEN READY
+    subprocess.run(["systemctl", "poweroff"])
+
+def main():
+    context = pyudev.Context()
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by(subsystem='usb')
+
+    observer = pyudev.MonitorObserver(monitor, device_event)
+    observer.start()
+
+    try:
+        print("Monitoring USB devices. Press Ctrl+C to exit.")
+        observer.join()
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
+
+if __name__ == "__main__":
+    main()
+
